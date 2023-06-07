0376d9fbc8dd6ae57dcc39dd46a900caf809a61a
initial@gmail.com <teamdailypractice@gmail.com>
2023-06-07 08:03:37 +0530

    initial grocery list app

---
+++ grocery-list-app/readme.md
@@ -0,0 +1,6 @@
+# Grocery list app
+
+## Notes
+
+* [JSON in HTML](https://www.freecodecamp.org/news/how-to-read-json-file-in-javascript/)
+
---
+++ grocery-list-app/scripts/index.js
@@ -0,0 +1,17 @@
+const items = [
+    {
+        "id": 1001,
+        "tamil":"பச்சரிசி",
+        "tamil_transliteration":"pacha arisi",
+        "english": "Raw Rice"
+    },
+    {
+        "id": 1002,
+        "tamil":"புழுங்கல் அரிசி",
+        "tamil_transliteration":"Pulungal Arisi",
+        "english": "Boiled Rice"
+    }
+];
+
+
+
